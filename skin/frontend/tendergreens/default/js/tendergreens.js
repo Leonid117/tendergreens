fbe88ffdae62238e15969cf83f591e4cf17b6db8
Leonid <Leonid.v117@gmail.com>
2018-03-11 22:46:26 +0300

    header 0

---
+++ skin/frontend/tendergreens/default/js/tendergreens.js
@@ -0,0 +1,16 @@
+/**
+ * JS for tender-greens theme.
+ */
+
+// Hamburger-menu in Header
+
+jQuery( document ).ready(function() {
+    (function () {
+        jQuery('.hamburger-menu').on('click', function() {
+            jQuery('.bar').toggleClass('animate');
+        })
+    })();
+});
+
+// END Hamburger-menu in Header
+
