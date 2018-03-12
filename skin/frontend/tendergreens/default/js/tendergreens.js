3590efde28d8608bc8a76759641681a50ea7eb1b
Leonid <Leonid.v117@gmail.com>
2018-03-12 15:04:00 +0300

    slider

--- skin/frontend/tendergreens/default/js/tendergreens.js
+++ skin/frontend/tendergreens/default/js/tendergreens.js
@@ -2,15 +2,94 @@
  * JS for tender-greens theme.
  */
 
-// Hamburger-menu in Header
+
 
 jQuery( document ).ready(function() {
+
+    // Hamburger-menu in Header
+
     (function () {
         jQuery('.hamburger-menu').on('click', function() {
             jQuery('.bar').toggleClass('animate');
         })
     })();
+
+    // END Hamburger-menu in Header
+
+    //Show Search
+
+    (function () {
+        jQuery('#search-icon').on('click', function() {
+            jQuery('#header-search').toggleClass('hide');
+        })
+    })();
+
+    // ENDShow Search
+
+    //Main-page slider
+
+    jssor_1_slider_init = function() {
+
+        var jssor_1_options = {
+            $AutoPlay: 1,
+            $SlideDuration: 800,
+            $SlideEasing: $Jease$.$OutQuint,
+            $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+            },
+            $BulletNavigatorOptions: {
+                $Class: $JssorBulletNavigator$
+            }
+        };
+
+        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+        /*#region responsive code begin*/
+
+        var MAX_WIDTH = 3000;
+
+        function ScaleSlider() {
+            var containerElement = jssor_1_slider.$Elmt.parentNode;
+            var containerWidth = containerElement.clientWidth;
+
+            if (containerWidth) {
+
+                var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+                jssor_1_slider.$ScaleWidth(expectedWidth);
+            }
+            else {
+                window.setTimeout(ScaleSlider, 30);
+            }
+        }
+
+        ScaleSlider();
+
+        $Jssor$.$AddEvent(window, "load", ScaleSlider);
+        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+        /*#endregion responsive code end*/
+    };
+
+    //END Main-page slider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
-// END Hamburger-menu in Header
+
 
