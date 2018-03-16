3091d4a77ccb8e43ba7114614b9199cf84435cb7
Leonid <Leonid.v117@gmail.com>
2018-03-16 16:56:29 +0300

    complete-front

--- skin/frontend/tendergreens/default/js/tendergreens.js
+++ skin/frontend/tendergreens/default/js/tendergreens.js
@@ -4,13 +4,14 @@
 
 
 
-jQuery( document ).ready(function() {
+jQuery(document).ready(function () {
 
     // Hamburger-menu in Header
 
     (function () {
-        jQuery('.hamburger-menu').on('click', function() {
+        jQuery('.hamburger-menu').on('click', function () {
             jQuery('.bar').toggleClass('animate');
+            jQuery('#tg-menu').toggleClass('hide');
         })
     })();
 
@@ -19,77 +20,43 @@ jQuery( document ).ready(function() {
     //Show Search
 
     (function () {
-        jQuery('#search-icon').on('click', function() {
+        jQuery('#search-icon').on('click', function () {
             jQuery('#header-search').toggleClass('hide');
         })
     })();
 
     // ENDShow Search
 
-    //Main-page slider
-
-    jssor_1_slider_init = function() {
-
-        var jssor_1_options = {
-            $AutoPlay: 1,
-            $SlideDuration: 800,
-            $SlideEasing: $Jease$.$OutQuint,
-            $ArrowNavigatorOptions: {
-                $Class: $JssorArrowNavigator$
-            },
-            $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$
-            }
-        };
-
-        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-
-        /*#region responsive code begin*/
-
-        var MAX_WIDTH = 3000;
-
-        function ScaleSlider() {
-            var containerElement = jssor_1_slider.$Elmt.parentNode;
-            var containerWidth = containerElement.clientWidth;
-
-            if (containerWidth) {
-
-                var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
-
-                jssor_1_slider.$ScaleWidth(expectedWidth);
-            }
-            else {
-                window.setTimeout(ScaleSlider, 30);
-            }
-        }
-
-        ScaleSlider();
-
-        $Jssor$.$AddEvent(window, "load", ScaleSlider);
-        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-        /*#endregion responsive code end*/
-    };
-
-    //END Main-page slider
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (jQuery(window).width() < 1200) {
+        jQuery('#tg-menu').append(jQuery('#tg-login').addClass('displace'));
+    }
+    if (jQuery(window).width() < 770) {
+        jQuery('#tg-menu').prepend(jQuery('#header-search').addClass('mobile-show'));
+    }
 
 
 
 
 });
+window.onload=function(){
+    jQuery('.5days .donut').viewportChecker({
+        classToAdd: 'donut--25', // Class to add to the elements when they are visible
+        offset: 100
+    });
+    jQuery('.14days .donut').viewportChecker({
+        classToAdd: 'donut--50', // Class to add to the elements when they are visible
+        offset: 100
+    });
 
+
+    var mh = 0;
+    jQuery(".printing-category-grid-container .col-6").each(function () {
+        var h_block = parseInt(jQuery(this).height());
+        if(h_block > mh) {
+            mh = h_block;
+        };
+    });
+    jQuery(".printing-category-grid-container .col-6").height(mh);
+};
 
 
